2d6d2dda3ae03fc98ce68b04a58cbc79d3a5dee1
AhmedMedhat64 <Ahmed.L2089619@gmail.com>
2025-06-27 15:14:22 +0300

    Commit

--- MainProject/Controllers/ProductsController.cs
+++ MainProject/Controllers/ProductsController.cs
@@ -24,5 +24,46 @@ namespace MainProject.Controllers
             _dbcontext.SaveChanges();
             return Ok(product.Id);
         }
+        [HttpPut]
+        [Route("")]
+        public ActionResult UpdateProduct(Product product)
+        {
+            var existingProduct = _dbcontext.Set<Product>().Find(product.Id);
+            if (existingProduct == null) {
+                return NotFound();
+            }
+            existingProduct.Name = product.Name;
+            existingProduct.Sku = product.Sku;
+            _dbcontext.Set<Product>().Update(existingProduct);
+            _dbcontext.SaveChanges();
+            return Ok();
+        }
+        [HttpGet]
+        [Route("")]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            var records = _dbcontext.Set<Product>().ToList();
+            return Ok(records);
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult GetById(int id)
+        {
+            var record = _dbcontext.Set<Product>().Find(id);
+            return record == null ? NotFound() : Ok(record);
+        }
+        [HttpDelete]
+        [Route("{id}")]
+        public ActionResult DeleteProduct(int id)
+        {
+            var existingProduct = _dbcontext.Set<Product>().Find(id);
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+            _dbcontext.Set<Product>().Remove(existingProduct);
+            _dbcontext.SaveChanges();
+            return Ok();
+        }
     }
 }
